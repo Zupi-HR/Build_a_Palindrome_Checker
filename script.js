408890bc2682fbb11662d45f35bdc604a1ec57bb
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2024-04-09 10:10:11 +0200

    finished

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const result = document.getElementById('result');
 
 checkBTN.addEventListener('click', function (e) {
     e.preventDefault();
-   /* const formattedString = textInput.value.trim(); */
    const formattedString = textInput.value.replace(/[^a-z0-9]/gi,'');
    console.log(formattedString);
    if(formattedString.length === 0) {
