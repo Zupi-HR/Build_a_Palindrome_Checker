7923dd9079bc0d78be81c389bc5db468cce0f1bc
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2024-04-09 10:09:39 +0200

    finished

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+
 const textInput = document.getElementById('text-input');
 const checkBTN = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
 checkBTN.addEventListener('click', function (e) {
     e.preventDefault();
-    result.style.display = 'block';
-    if (textInput.value.trim().length === 0) {
-        alert('Please input a value');
-    } else {
-        result.textContent = `${textInput.value} is a palindrome`;
-    }
+   /* const formattedString = textInput.value.trim(); */
+   const formattedString = textInput.value.replace(/[^a-z0-9]/gi,'');
+   console.log(formattedString);
+   if(formattedString.length === 0) {
+    alert('Please input a value');
+    return;
+   }
+   let reversedString = '';
+   for (let index = 0; index < formattedString.length; index++) {
+    reversedString += formattedString.at(formattedString.length - index - 1);
+   }
+   result.style.display = 'block';
+   console.log(reversedString);
+   if (reversedString.toLowerCase() === formattedString.toLowerCase()) {
+    result.textContent = `${textInput.value} is a palindrome`;
+   } else {
+    result.textContent = `${textInput.value} is not a palindrome`;
+   }
 });
